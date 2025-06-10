47312ce06013280cdf9bd5100e954c40969b9f2e
Nowhere0p <rajatchaudhary6399@gmail.com>
2025-06-10 21:38:06 +0530

    #ff0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-         System.out.print("$ ");
-
+         while (true){
+            handleCommand();
+         }
+    }
+    public  static  void handleCommand(){
+        System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         System.out.println(input+": command not found");
