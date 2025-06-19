84597c58ef21a25ada569b1f7aed466b67288a1c
Nowhere0p <rajatchaudhary6399@gmail.com>
2025-06-19 22:13:39 +0530

    #ip1

--- src/main/java/server/CommandHandler.java
+++ src/main/java/server/CommandHandler.java
@@ -2,8 +2,11 @@ package server;
 
 import commands.Command;
 import parser.ParsedCommand;
+import util.CommandUtils;
 
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public class CommandHandler {
     public static void handle(ParsedCommand parsedCommand, Map<String, Command> commands) {
@@ -11,7 +14,40 @@ public class CommandHandler {
         if(commands.containsKey(parsedCommand.command)){
             commands.get(parsedCommand.command).execute(parsedCommand);
         }else {
-            System.out.println(parsedCommand.command + ": command not found");
+            List<String> commandLocations = CommandUtils.checkCommandInPath(parsedCommand.command);
+            if(commandLocations.isEmpty()){
+                System.out.println(parsedCommand.command + ": command not found");
+            } 
+            try{
+                var statusCode=runExecutable(parsedCommand,commandLocations);
+                if(statusCode!=0){
+                    System.out.println(parsedCommand.command+": command failed");
+                }
+            }
+            catch(IOException | InterruptedException e){
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+
         }
+    }
+
+    private static int runExecutable(ParsedCommand parsedCommand, List<String> commandLocations) throws IOException,InterruptedException {
+      if(commandLocations.size()>1){
+        System.out.println("Multiple executables found");
+      }
+        var executable=commandLocations.getFirst();
+        File executabFile=new File(executable);
+        if(!executabFile.exists() ||!executabFile.canExecute()){
+           return 1;
+        }
+        ProcessBuilder builder=new ProcessBuilder(executabFile.getName());
+        builder.command().addAll(parsedCommand.args);
+        builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectInput(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectError(ProcessBuilder.Redirect.INHERIT);
+        var process=builder.start();
+        return process.waitFor();
+
     }
 }
--- src/main/java/util/CommandUtils.java
+++ src/main/java/util/CommandUtils.java
@@ -5,11 +5,11 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class CommandUtils {
-public static List<String> checkCommandInPath(String arg){
+public static List<String> checkCommandInPath(String command){
     List<String> paths=getPaths();
     List<String> locations=new ArrayList<>();
     for(String dir: paths) {
-        File executable = new File(dir, arg);
+        File executable = new File(dir, command);
         if (executable.exists() && executable.canExecute()) {
             locations.add(executable.getAbsolutePath());
         }
